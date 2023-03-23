7ea9a9e3cb7c5565ac0967438d6e7a98f51e3898
amelyzov <asdfqwefqwef@mail.ru>
2023-03-23 17:05:47 +0300

    fixed database

--- 11111sank.py
+++ 11111sank.py
@@ -1,12 +1,37 @@
-import sqlite3
-from contextlib import closing
-import sankey.prepare_data_new
+import plotly.graph_objects as go
+import requests, json
 
-db = 'sankey_base.db'
+url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
+response = requests.get(url)
+data = response.json()
 
+# override gray link colors with 'source' colors
+opacity = 0.4
+# change 'magenta' to its 'rgba' value to add opacity
+data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
+data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
+                                    for src in data['data'][0]['link']['source']]
 
-with closing(sqlite3.connect(db)) as connection:
-    cursor = connection.cursor()
-    cursor.execute("""select node_name, id from node """)
-    node = dict(cursor.fetchall())
-    print(node)
+fig = go.Figure(data=[go.Sankey(
+    valueformat = ".0f",
+    valuesuffix = "TWh",
+    # Define nodes
+    node = dict(
+      pad = 25,
+      thickness = 15,
+      line = dict(color = "black", width = 0.5),
+      label =  data['data'][0]['node']['label'],
+      color =  data['data'][0]['node']['color']
+    ),
+    # Add links
+    link = dict(
+      source =  data['data'][0]['link']['source'],
+      target =  data['data'][0]['link']['target'],
+      value =  data['data'][0]['link']['value'],
+      label =  data['data'][0]['link']['label'],
+      color =  data['data'][0]['link']['color']
+))])
+
+fig.update_layout(title_text="Energy forecast for 2050<br>Source: Department of Energy & Climate Change, Tom Counsell via <a href='https://bost.ocks.org/mike/sankey/'>Mike Bostock</a>",
+                  font_size=10)
+fig.show()
--- 222.py
+++ 222.py
@@ -1,12 +1,19 @@
+import time
+import aiohttp
+
 import sankey.prepare_data_new as snk
 from sdk import SedmaxHeader, Sedmax, ElectricalArchive
 
 s = Sedmax('https://demo.sedmax.ru')
 
+req = {'channels': ['el-dev-101-ea_imp-30m', 'el-dev-102-ea_imp-30m'],
+       'begin': '2022-02-15 05:00:00', 'end': '2023-03-03 05:00:00'}
+
 username = 'demo'  # os.environ['SEDMAX_USERNAME']
 password = 'demo'  # os.environ['SEDMAX_PASSWORD']
 s.login(username, password)
-snk.load_data(s,'2023-03-01 00:30:00', '2023-03-02 00:30:00')
-# request = snk.prepare_arch_request(s.channel.index.tolist(), '2023-03-01 00:30:00', '2023-03-02 00:30:00')
-# print('тута', request)
 
+
+resp = snk.load_data(s, '2023-03-01 00:30:00', '2023-03-02 00:30:00')
+print(s.node.keys())
+print(resp)
