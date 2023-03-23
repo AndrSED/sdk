24c84d89f33f1758fb63a91983c2a31fc387c4d8
amelyzov <asdfqwefqwef@mail.ru>
2023-03-23 17:09:19 +0300

    fixed database

--- settings.py
+++ settings.py
@@ -2,17 +2,6 @@ from enum import Enum
 
 SANKEY_DATABASE = 'sankey_base.db'
 
-# colors = {
-#     'grid': "#b9b9b9",
-#     'graph_font': "#000000",
-#     'plot_area': "#ffffff",
-#     'plot_background': "#fafafa",
-#     'active_link': 'rgba(200,200,200,0.7)',
-#     'disabled_link': 'rgba(250,100,100,0.7)',
-#     'node': 'rgba(200,200,200,0.7)',
-# }
-
-
 class SankeyColor(Enum):
     GRAPH_FONT = "#000000"
     PLOT_BACKGROUND = "#fafafa"
